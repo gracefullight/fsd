5994a800e348d4ab4f9df4630c8a4bea7dcd0c4b
gracefullight <gracefullight.dev@gmail.com>
2025-08-12 17:04:44 +1000

    feat: week3

---
+++ src/week3/main.py
@@ -0,0 +1,39 @@
+from random import randint
+from typing import Any
+
+
+class Customer:
+    def __init__(self, name: str, gender: str) -> None:
+        self.customerId = randint(1, 100)
+        self.name = name
+        self.gender = gender
+        self.accounts: list[Any] = []
+
+    def login(self) -> None:
+        pass
+
+    def logout(self) -> None:
+        pass
+
+
+class BankAccount:
+    def __init__(self, account_type: str) -> None:
+        self.balance = 0
+        self.type = account_type
+
+    def withdraw(self, amount: int) -> None:
+        self.balance -= amount
+
+    def deposit(self, amount: int) -> None:
+        self.balance += amount
+
+    def transfer(self, amount: int, target_account: "BankAccount") -> None:
+        if self.balance >= amount:
+            self.withdraw(amount)
+            target_account.deposit(amount)
+
+    def show_balance(self) -> None:
+        print(self.balance)
+
+
+bank_a = BankAccount("Saving")
