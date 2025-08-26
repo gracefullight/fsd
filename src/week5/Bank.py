add3483fe0d9df90a2dfd4cb20f66fc7a4958c5f
gracefullight <gracefullight.dev@gmail.com>
2025-08-26 17:04:42 +1000

    feat: fsd 005

---
+++ src/week5/Bank.py
@@ -0,0 +1,43 @@
+class Bank:
+    def __init__(self) -> None:
+        self.balance = 1000.0
+
+    def deposit(self) -> None:
+        deposit_amount = float(input("Enter deposit amount: "))
+        self.balance += deposit_amount
+        print(f"Amount ${deposit_amount:.2f} deposited successfully.")
+
+    def withdraw(self) -> None:
+        withdraw_amount = float(input("Amount of withdrawal: "))
+        if self.balance >= withdraw_amount:
+            self.balance -= withdraw_amount
+            print(f"Amount ${withdraw_amount:.2f} withdrawn successfully.")
+        else:
+            print("Insufficient funds.")
+
+    def show_balance(self) -> None:
+        print(f"Current balance is ${self.balance:.2f}")
+
+    def help_message(self) -> None:
+        print(
+            "Please type following command:\nd - deposit\nw - withdraw\ns - show balance\nx - exit"
+        )
+
+    def get_users_input(self) -> None:
+        pass
+
+    def main(self) -> None:
+        user_input = input("start banking (d/w/s/x):").strip().lower()
+        while user_input != "x":
+            match user_input:
+                case "d":
+                    self.deposit()
+                case "w":
+                    self.withdraw()
+                case "s":
+                    self.show_balance()
+                case _:
+                    self.help_message()
+            user_input = input("start banking (d/w/s/x):").strip().lower()
+        # Finish while loop, then good bye
+        print("Thanks for using us! Good bye!")
